09e08039461a6f821a2adfec55c9aebbaf4fcf39
Jiayuan Mao <maojiayuan@gmail.com>
2019-01-26 17:31:09 -0500

    [pytorch1.0] move to pytorch1.0

--- pytorch/prroi_pool/travis.sh
+++ pytorch/prroi_pool/travis.sh
@@ -6,13 +6,7 @@
 # Distributed under terms of the MIT license.
 # Copyright (c) 2017 Megvii Technology Limited.
 
-cd src
-echo "Working directory: " `pwd`
-echo "Compiling prroi_pooling kernels by nvcc..."
-nvcc -c -o prroi_pooling_gpu_impl.cu.o prroi_pooling_gpu_impl.cu -x cu -Xcompiler -fPIC -arch=sm_52
-
-cd ../
 echo "Working directory: " `pwd`
 echo "Building python libraries..."
-python3 build.py
+python3 setup.py build_ext
 
